cb56d7e6cd916cda3f15ebc52676edb59867c8e2
SkyElysium <2817842285@qq.com>
2026-02-08 13:49:24 +0800

    Normal code style change

--- TextEditor/core/editor.py
+++ TextEditor/core/editor.py
@@ -13,21 +13,27 @@ class Editor(tk.Tk):
 
         self.surface()
 
-        self.bind('<Control-n>', self.custom_notebook.add_tab)
-        self.bind('<Control-N>', self.custom_notebook.add_tab)
-        self.bind('<Control-o>', self.custom_notebook.open)
-        self.bind('<Control-O>', self.custom_notebook.open)
-        self.bind('<Control-s>', self.custom_notebook.save)
-        self.bind('<Control-S>', self.custom_notebook.save)
-        self.bind('<Control-Alt-s>', self.custom_notebook.save_as)
-        self.bind('<Control-Alt-S>', self.custom_notebook.save_as)
-        self.bind('<Control-F4>', self.custom_notebook.remove_tab)
+        # Format: the shortcuts and then method.
+        binding_dict = {
+            '<Control-n>'    : self.custom_notebook.add_tab,
+            '<Control-o>'    : self.custom_notebook.open,
+            '<Control-s>'    : self.custom_notebook.save,
+            '<Control-Alt-s>': self.custom_notebook.save_as,
+            '<Control-F4>'   : self.custom_notebook.remove_tab,
+        }
+
+        for shortcut, method in binding_dict.items():
+            # If not the first letter each word is upper,
+            # that means two kinds of a shortcut: upper and lower.
+            if not shortcut.istitle(): self.bind(shortcut.title(), method)
+
+            self.bind(shortcut, method)
 
     def surface(self):
         self.custom_notebook = CustomNotebook(main_window = self)
         self.custom_notebook.pack(fill = 'both', expand = True)
 
-        self.main_menu = MainMenu(nb_obj = self.custom_notebook)
+        self.main_menu = MainMenu(main_notebook = self.custom_notebook)
         self.config(menu = self.main_menu)
 
         self.custom_notebook.add_tab()
