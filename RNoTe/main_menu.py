c302974bbbd17ccf8a23d1f8e3f6d70c2f53f365
SkyElysium <2817842285@qq.com>
2026-05-15 21:53:49 +0800

    主菜单问题修复

---
+++ RNoTe/main_menu.py
@@ -0,0 +1,220 @@
+from typing import Optional
+
+import tkinter as tk
+import tkinter.ttk as ttk
+
+import webbrowser
+
+from core.config import *
+
+
+class MainMenu(tk.Menu):
+    def __init__(self, master: tk.Misc) -> None:
+
+        super().__init__(master)
+
+        self.main_notebook = master.custom_notebook
+        self.master = master
+
+        self.font_size = tk.IntVar(self, 13)
+        self.font_size.trace('w', self._change_font_size)
+
+        self['postcommand'] = self._change_status_of_options
+
+        # Options that need checking the status in "_change_status_of_options"
+        self.file_option_checklist = ['关闭', '保存', '另存为...']
+        self.edit_option_checklist = ['撤销', '重做', '复制', '剪切', '粘贴', '全选']
+        self.view_option_checklist = ['放大', '缩小', '恢复默认大小']
+
+        # File
+        self.file_option = tk.Menu(
+            self,
+            tearoff = False,
+            activeforeground = 'black',
+            activebackground = '#91c9f7'
+        )
+
+        self.file_option.add_command(
+            label = '新建',
+            accelerator = 'Ctrl+N',
+            command = self.main_notebook.add_tab
+        )
+        self.file_option.add_separator()
+        self.file_option.add_command(
+            label = '打开',
+            accelerator = 'Ctrl+O',
+            command = self.main_notebook.open_file
+        )
+        self.file_option.add_command(
+            label = '保存',
+            accelerator = 'Ctrl+S',
+            command = self.main_notebook.save_file
+        )
+        self.file_option.add_command(
+            label = '另存为...',
+            accelerator = 'Ctrl+Alt+S',
+            command = self.main_notebook.save_file_as
+        )
+        self.file_option.add_separator()
+        self.file_option.add_command(
+            label = '关闭',
+            accelerator = 'Ctrl+F4',
+            command = self.main_notebook.safely_close_file
+        )
+        self.file_option.add_separator()
+        self.file_option.add_command(
+            label = '退出',
+            accelerator = 'Alt+F4',
+            command = self.master.exiting
+        )
+
+        self.add_cascade(label = '文件', menu = self.file_option)
+
+        # Edit
+        self.edit_option = tk.Menu(
+            self,
+            tearoff = False,
+            activeforeground = 'black',
+            activebackground = '#91c9f7'
+        )
+
+        self.edit_option.add_command(
+            label = '撤销',
+            accelerator = 'Ctrl+Z',
+            command = lambda : self.main_notebook.get_tab()[1].undo()
+        )
+        self.edit_option.add_command(
+            label = '重做',
+            accelerator = 'Ctrl+Y',
+            command = lambda : self.main_notebook.get_tab()[1].redo()
+        )
+        self.edit_option.add_separator()
+        self.edit_option.add_command(
+            label = '复制',
+            accelerator = 'Ctrl+C',
+            command = lambda : self.main_notebook.get_tab()[1].copy()
+        )
+        self.edit_option.add_command(
+            label = '剪切',
+            accelerator = 'Ctrl+X',
+            command = lambda : self.main_notebook.get_tab()[1].cut()
+        )
+        self.edit_option.add_command(
+            label = '粘贴',
+            accelerator = 'Ctrl+V',
+            command = lambda : self.main_notebook.get_tab()[1].paste()
+        )
+        self.edit_option.add_separator()
+        self.edit_option.add_command(
+            label = '全选',
+            accelerator = 'Ctrl+A',
+            command = lambda : self.main_notebook.get_tab()[1].select_all()
+        )
+
+        self.add_cascade(label = '编辑', menu = self.edit_option)
+
+        # View
+        self.view_option = tk.Menu(
+            self,
+            tearoff = False,
+            activeforeground = 'black',
+            activebackground = '#91c9f7'
+        )
+
+        self.view_option.add_command(
+            label = '放大',
+            accelerator = 'Ctrl++',
+            command = self.zoom_in_font
+        )
+        self.view_option.add_command(
+            label = '缩小',
+            accelerator = 'Ctrl+-',
+            command = self.zoom_out_font
+        )
+        self.view_option.add_command(
+            label = '恢复默认大小',
+            command = lambda : self.font_size.set(13)
+        )
+
+        self.add_cascade(label = '视图', menu = self.view_option)
+
+        # About
+        self.about_option = tk.Menu(
+            self,
+            tearoff = False,
+            activeforeground = 'black',
+            activebackground = '#91c9f7'
+        )
+
+        self.about_option.add_command(
+            label = '关于',
+            command = self._popup_about_dialog
+        )
+        self.about_option.add_command(
+            label = '报告问题',
+            command = self._link_to_issue
+        )
+
+        self.add_cascade(label = '关于', menu = self.about_option)
+
+    def _change_status_of_options(self) -> None:
+
+        try:
+            self.master.clipboard_get()
+            self.edit_option.entryconfig('粘贴', state = 'normal')
+        except tk.TclError:
+            self.edit_option.entryconfig('粘贴', state = 'disabled')
+
+        status = 'disabled' if not self.main_notebook.tabs() else 'normal'
+
+        for each in self.file_option_checklist:
+            self.file_option.entryconfig(each, state = status)
+        for each in self.edit_option_checklist:
+            self.edit_option.entryconfig(each, state = status)
+        for each in self.view_option_checklist:
+            self.view_option.entryconfig(each, state = status)
+
+    def _change_font_size(self, *args) -> None:
+
+        for tab_id in self.main_notebook.tabs():
+            tab = self.main_notebook.nametowidget(tab_id)
+
+            tab.line_number_bar.config(font = ('Consolas', self.font_size.get()))
+            tab.text.config(font = ('Consolas', self.font_size.get()))
+
+    def zoom_in_font(self, event: Optional[tk.Event] = None) -> None:
+
+        if self.font_size.get() == 60: return
+
+        self.font_size.set(self.font_size.get() + 1)
+
+    def zoom_out_font(self, event: Optional[tk.Event] = None) -> None:
+
+        if self.font_size.get() == 1: return
+
+        self.font_size.set(self.font_size.get() - 1)
+
+    def _popup_about_dialog(self) -> None:
+
+        dialog = tk.Toplevel()
+        dialog.title('关于RNoTe')
+        dialog.iconphoto(True, tk.PhotoImage(file = 'data/icon.png'))
+
+        x, y = self.master.winfo_x(), self.master.winfo_y()
+        dialog.geometry('350x120')
+        dialog.geometry(f'+{x + 200}+{y + 200}')
+
+        self.master.wm_attributes('-disabled', True)
+        dialog.bind('<Destroy>', lambda _: self.master.wm_attributes('-disabled', False))
+
+        dialog.resizable(False, False)
+        dialog.focus()
+
+        tk.Label(dialog, text = MAIN_WINDOW_TITLE, font = ('Consolas', 15)).pack()
+        ttk.Separator(dialog).pack(fill = 'x')
+        tk.Message(dialog, text = FIRST_INFO, width = 600).pack()
+        tk.Message(dialog, text = SECOND_INFO, width = 600, justify = 'center').pack(side = 'bottom')
+
+    def _link_to_issue(self) -> None:
+
+        webbrowser.open(ISSUE_URL)
